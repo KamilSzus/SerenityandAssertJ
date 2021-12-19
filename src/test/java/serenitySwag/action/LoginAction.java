3cd1b0bd28e5ee7e1e8b722deafabd26c10c476c
KamilSzus <kamil-szustak1@wp.pl>
2021-12-19 15:25:16 +0100

    Example how to find element on page by id

--- src/test/java/serenitySwag/action/LoginAction.java
+++ src/test/java/serenitySwag/action/LoginAction.java
@@ -3,14 +3,16 @@ package serenitySwag.action;
 import net.serenitybdd.core.steps.UIInteractionSteps;
 import net.thucydides.core.annotations.Step;
 
+//Example how to find element in by using id
+
 public class LoginAction extends UIInteractionSteps {
 
     @Step("Login as {0}")
     public void loginAS(User user){
         openUrl("https://www.saucedemo.com/");
 
-        $(("[data-test='username']")).sendKeys(user.getUserLogin());
-        $(("[data-test='password']")).sendKeys(user.getUserPassword());
-        $(("[data-test='login-button']")).click();
+        $("#user-name").sendKeys(user.getUserLogin());
+        $("#password").sendKeys(user.getUserPassword());
+        $("#login-button").click();
     }
 }
